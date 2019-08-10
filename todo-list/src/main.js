a3c77cccd8b7737faef416a836f2d87227ccf963
cesar sousa <cesarsonline@gmail.com>
2019-08-10 12:10:55 -0300

    definicao das rotas

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -3,11 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
+// ========================
+// Router
+// ========================
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
+  store,
+  router,
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  template:
+    <div id="app">
+      <transition name="fade"  mode="out-in">
+        <router-view class="view"></router-view>
+      </transition>
+    </div>
+	,
+  components: { App }
 })
---
+++ todo-list/src/routes.js
@@ -0,0 +1,8 @@
+
+import App from './App.vue'
+import CepChecker from './CepChecker.vue'
+
+export default [
+{ path: '/', component: App },
+{ path: '/cep', component: CepChecker }
+]
