d7c1caf8572c18e676473cf57bea49b25cf2db3a
cesar sousa <cesarsonline@gmail.com>
2019-08-11 15:52:28 -0300

    ajustando cor de fundo da tela /cep

--- todo-list/src/CepChecker.vue
+++ todo-list/src/CepChecker.vue
@@ -15,6 +15,18 @@ return {
 </script>
 
 <style lang="less">
+body {
+	font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	line-height: 1.4em;
+	background: #f5f5f5;
+	color: #4d4d4d;
+	min-width: 230px;
+	max-width: 550px;
+	margin: 0 auto;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	font-weight: 300;
+}
 .home{
   text-decoration: none;
   font-size: 16px;
