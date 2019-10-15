f3467eef91914f87a2c37b07125e3bb99c7f1d53
cesar sousa <cesarsonline@gmail.com>
2019-10-14 23:18:07 -0300

    atualizando estrutura de arquivos

---
+++ cap_10_servicos/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <lv-lista titulo="Carros" :lista="carros"></lv-lista>
+    <lv-lista titulo="Aviões" :lista="avioes"></lv-lista>
+  </div>
+</template>
+
+<script>
+import Carro from './servicos/carros'
+import Aviao from './servicos/avioes'
+import LvLista from './LvLista.vue'
+
+export default {
+  name: 'app',
+  data() { return { carros: {}, avioes: {} } },
+  components: {LvLista},
+  mounted() {
+    Carro.lista().then(dado => this.carros = dado.data)
+
+    Aviao.lista().then(dado => this.avioes = dado.data)
+  }
+}
+</script>
